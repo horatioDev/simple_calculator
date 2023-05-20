d62796e3f42d922cddd4617ad3eda62e78feaef8
Ray-Xavier-2021 <rayxavier.frontenddev.2021@gmail.com>
2023-05-20 18:38:57 -0400

    Upload files from desktop 5/20/2023

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
 // Simple Calcutator: HTML, CSS: grid, and JS
 
 // Grab elements
-const output = document.getElementById('output');
-const button = document.querySelectorAll('#buttons');
+var output = document.getElementById('output');
+const buttons = document.querySelectorAll('button[class="btn"]');
 
 // Add event listener
-button.addEventListener('click', displayOutput);
+// button.addEventListener('click', displayOutput);
+
+// High order function to loop through buttons and catch the value 
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        let results = output.innerText = e.target.value;
+        output += results;
+        // return results;
+        console.log(results);
+    });
+});
 
 // Create a display function
 function displayOutput(e) {
-    console.log('123');
+    console.log(e.target);
 };
